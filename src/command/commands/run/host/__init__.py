e924b3a552a9c20719c58cc98e001e8bca6417c3
Greg Bruno <greg.bruno@gmail.com>
2010-04-15 19:00:30 +0000

    'rocks run host' is now 100% tentakel free!

--- src/command/commands/run/host/__init__.py
+++ src/command/commands/run/host/__init__.py
@@ -1,4 +1,4 @@
-# $Id: __init__.py,v 1.9 2010/02/24 22:04:25 bruno Exp $
+# $Id: __init__.py,v 1.10 2010/04/15 19:00:30 bruno Exp $
 #
 # @Copyright@
 # 
@@ -54,6 +54,9 @@
 # @Copyright@
 #
 # $Log: __init__.py,v $
+# Revision 1.10  2010/04/15 19:00:30  bruno
+# 'rocks run host' is now 100% tentakel free!
+#
 # Revision 1.9  2010/02/24 22:04:25  bruno
 # added a 'timeout' parameter to 'rocks run host'. idea by Tim Carlson.
 #
@@ -87,29 +90,42 @@
 #
 #
 
+import threading
 import os
+import sys
+import time
+import socket
+import popen2
 import rocks.commands
 
-import lekatnet.config as config
-import lekatnet.remote as remote
-
-class RocksRemoteCollator(remote.RemoteCollator):
-	def __init__(self, cmd):
-		self.clear()
-		self.formatter = remote.FormatString()
+class Parallel(threading.Thread):
+	def __init__(self, cmdclass, cmd, host, stats, collate):
+		threading.Thread.__init__(self)
 		self.cmd = cmd
+		self.host = host
+		self.stats = stats
+		self.collate = collate
+		self.cmdclass = cmdclass
 
-	def displayAll(self):
-		"Display the next pending result for every remote object"
+	def run(self):
+		starttime = time.time()
+		#os.system(self.cmd)
+		child_stdout_and_stderr, child_stdin = popen2.popen4(self.cmd)
 
-		displayCount = len(self.remoteobjects)
-		while displayCount > 0:
-			obj = remote.RemoteCommand.finishedObjects.get()
-			displayCount -= 1
-			status, output = obj.getResult()
+		for line in child_stdout_and_stderr.readlines():
+			if self.collate:
+				self.cmdclass.addOutput(self.host, line[:-1])
+			else:
+				print line[:-1]
 
-			for line in output.split('\n'):
-				self.cmd.addOutput(obj.destination, line)
+		if self.stats:
+			msg = 'command on host %s took %f seconds' % \
+				(self.host, time.time() - starttime)
+
+			if self.collate:
+				self.cmdclass.addOutput(self.host, msg)
+			else:
+				print msg
 
 	
 class command(rocks.commands.HostArgumentProcessor,
@@ -147,6 +163,25 @@ class Command(command):
 	Default is '30'.
 	</arg>
 
+	<arg type='string' name='delay'>
+	Sets the time (in seconds) to delay between each executed command
+	on multiple hosts. For example, if the command is run on two
+	hosts and if the delay is 10, then the command will be executed on host
+	1, then 10 seconds later, the command will be executed on host 2.
+	Default is '0' (no delay).
+	</arg>
+
+	<arg type='string' name='stats'>
+	Display performance statistics if this parameter is set to 'yes'.
+	Default is 'no'.
+	</arg>
+
+	<arg type='string' name='collate'>
+	Prepend the hostname to every output line if this parameter is set to
+	'yes'.
+	Default is 'no'.
+	</arg>
+
 	<param type='string' name='command'>
 	Can be used in place of the 'command' argument.
 	</param>
@@ -166,13 +201,14 @@ class Command(command):
 		if not command:
 			self.abort('must supply a command')
 
-		(managed, x11, t) = self.fillParams([
+		(managed, x11, t, d, stats, collate) = self.fillParams([
 			('managed', 'y'),
 			('x11', 'y'),
-			('timeout', '30')
+			('timeout', '30'),
+			('delay', '0'),
+			('stats', 'n'),
+			('collate', 'n')
 			])
-
-		managed_only = self.str2bool(managed)
 
 		try:
 			timeout = int(t)
@@ -182,7 +218,12 @@ class Command(command):
 		if timeout <= 0:
 			self.abort('"timeout" must be a postive integer')
 
-		hosts = self.getHostnames(args, managed_only)
+		try:
+			delay = float(d)
+		except:
+			self.abort('"delay" must be a floating point number')
+
+		hosts = self.getHostnames(args, self.str2bool(managed))
 		
 		# This is the same as doing -x using ssh.  Might be useful
 		# for the common case, but required for the Viz Roll.
@@ -193,23 +234,81 @@ class Command(command):
 			except KeyError:
 				pass
 
-		conf = config.ConfigBase()
-		f = open('/etc/tentakel.conf')
-		conf.load(f)
-		f.close()
+		if self.str2bool(collate):
+			self.beginOutput()
 
-		dests = RocksRemoteCollator(self)
-		dests.format = '%o\\n'
-
-		params = conf.getGroupParams('default')
-		params['timeout'] = timeout
-
+		threads = []
 		for host in hosts:
-			dests.add(remote.remoteCommandFactory(host, params))
+			#
+			# first test if the node is up and responding to ssh
+			#
+			sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			sock.settimeout(2)
+			try:
+				#
+				# this catches the case when the host is down
+				# and/or there is no ssh daemon running
+				#
+				sock.connect((host, 22))
 
-		dests.execAll(command)
+				#
+				# this catches the case when the node is up,
+				# sshd is sitting on port 22, but it is not
+				# responding (e.g., the node is overloaded,
+				# sshd is hung, etc.)
+				#
+				# sock.recv() should return something like:
+				#
+				#	SSH-2.0-OpenSSH_4.3
+				#
+				buf = sock.recv(64)
+			except socket.error:
+				if self.collate:
+					self.addOutput(host, 'down')
+				else:
+					print '%s: down'		
 
-		self.beginOutput()
-		dests.displayAll()
-		self.endOutput(padChar='')
-		
+				continue
+
+			cmd = 'ssh %s "%s"' % (host, sys.argv[-1])
+
+			p = Parallel(self, cmd, host, self.str2bool(stats),
+				self.str2bool(collate))
+			p.start()
+			threads.append(p)
+
+			if delay > 0:
+				time.sleep(delay)
+
+		#
+		# collect the threads
+		#
+		while 1:
+			active = threading.enumerate()
+
+			if len(active) == 1:
+				break
+				
+			#
+			# special case for the last thread (this program is
+			# thread '1')
+			#
+			if len(active) == 2:
+				active[1].join(timeout)
+				break
+
+			t = threads
+			for thread in t:
+				if thread not in active:
+					thread.join(0.1)
+					threads.remove(thread)
+
+			#
+			# don't burn a CPU while waiting for the threads to
+			# complete
+			#
+			time.sleep(0.5)
+
+		if self.str2bool(collate):
+			self.endOutput(padChar='')
+
