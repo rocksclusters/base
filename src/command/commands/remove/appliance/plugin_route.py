d0d2b0a349dbd43921c2dbae8e6d028ebd6b6ab8
Anoop Rajendra <anoop.rajendra@gmail.com>
2010-07-19 18:30:51 +0000

    Bug fix "rocks remove appliance" runs "rocks remove appliance route" plugin without any args. "remove appliance route" cannot be run without args, and "rocks remove appliance" fails. So now instead of calling the command, the plugin runs sql directly to remove appropriate entries from the appliance routes table;

--- src/command/commands/remove/appliance/plugin_route.py
+++ src/command/commands/remove/appliance/plugin_route.py
@@ -1,4 +1,4 @@
-# $Id: plugin_route.py,v 1.2 2009/05/01 19:06:59 mjk Exp $
+# $Id: plugin_route.py,v 1.3 2010/07/19 18:30:51 anoop Exp $
 # 
 # @Copyright@
 # 
@@ -54,6 +54,14 @@
 # @Copyright@
 #
 # $Log: plugin_route.py,v $
+# Revision 1.3  2010/07/19 18:30:51  anoop
+# Bug fix
+# "rocks remove appliance" runs "rocks remove appliance route" plugin
+# without any args. "remove appliance route" cannot be run without args,
+# and "rocks remove appliance" fails. So now instead of calling the command,
+# the plugin runs sql directly to remove appropriate entries from the
+# appliance routes table;
+#
 # Revision 1.2  2009/05/01 19:06:59  mjk
 # chimi con queso
 #
@@ -70,4 +78,6 @@ class Plugin(rocks.commands.Plugin):
 		return 'route'
 
 	def run(self, appliance):
-		self.owner.command('remove.appliance.route', [ appliance ])
+		self.owner.db.execute('delete from appliance_routes where ' +\
+			'appliance_routes.appliance=(select id from appliances ' +\
+			'where name="%s")' % appliance)
