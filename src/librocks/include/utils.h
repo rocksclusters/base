1c1c155d43ed7e1bcc51548e11856569122f0292
Mason Katz <mason.katz@gmail.com>
2010-10-18 17:24:36 +0000

    *** empty log message ***

---
+++ src/librocks/include/utils.h
@@ -0,0 +1,27 @@
+/* $Id: utils.h,v 1.1 2010/10/18 17:24:36 mjk Exp $
+ *
+ * @Copyright@
+ * @Copyright@
+ *
+ * $Log: utils.h,v $
+ * Revision 1.1  2010/10/18 17:24:36  mjk
+ * *** empty log message ***
+ *
+ */
+ 
+#ifndef _UTILS_H_
+#define _UTILS_H_
+ 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	void HexDump(FILE *, const char *, const char *, int);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+ 
+#endif /* __UTILS_H__ */
