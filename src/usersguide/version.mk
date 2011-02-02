8661a4b9507e7e7a30b468d5bd723719a8e1e196
Mason Katz <mason.katz@gmail.com>
2011-02-02 18:52:48 +0000

    Added Attr section Cleanup

--- src/usersguide/version.mk
+++ src/usersguide/version.mk
@@ -1,10 +1,11 @@
 NAME    = roll-$(ROLL)-usersguide
-RELEASE = 0
+RELEASE = 1
+RPM.ARCH = noarch
 
 SUMMARY_COMPATIBLE      = $(VERSION)
 SUMMARY_MAINTAINER      = Rocks Group
 SUMMARY_ARCHITECTURE    = i386, x86_64
 
-ROLL_REQUIRES           = base kernel os1 os2
+ROLL_REQUIRES           = base kernel service-pack os
 ROLL_CONFLICTS          =
 
