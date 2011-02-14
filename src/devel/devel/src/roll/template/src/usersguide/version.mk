5756fbe0f72c727585a71795c049c0dec7002925
Mason Katz <mason.katz@gmail.com>
2011-02-14 23:44:04 +0000

    fix template docbook

--- src/devel/devel/src/roll/template/src/usersguide/version.mk
+++ src/devel/devel/src/roll/template/src/usersguide/version.mk
@@ -7,6 +7,6 @@ SUMMARY_COMPATIBLE	= $(VERSION)
 SUMMARY_MAINTAINER	= Rocks Group
 SUMMARY_ARCHITECTURE	= i386, x86_64
 
-ROLL_REQUIRES		= base kernel web-server os1 os2
+ROLL_REQUIRES		= base kernel os
 ROLL_CONFLICTS		=
 
