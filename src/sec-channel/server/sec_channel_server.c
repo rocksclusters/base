a098ac8c44a6602ea6825f7c733958775827cc06
Anoop Rajendra <anoop.rajendra@gmail.com>
2011-04-20 05:55:04 +0000

    Fixed server to daemonize itself

--- src/sec-channel/server/sec_channel_server.c
+++ src/sec-channel/server/sec_channel_server.c
@@ -1,10 +1,13 @@
 /*
- * $Id: sec_channel_server.c,v 1.1 2011/04/11 22:41:50 anoop Exp $
+ * $Id: sec_channel_server.c,v 1.2 2011/04/20 05:55:04 anoop Exp $
  *
  * @Copyright@
  * @Copyright@
  *
  * $Log: sec_channel_server.c,v $
+ * Revision 1.2  2011/04/20 05:55:04  anoop
+ * Fixed server to daemonize itself
+ *
  * Revision 1.1  2011/04/11 22:41:50  anoop
  * RPC service that initiates transfer of shared 411 key
  *
@@ -20,9 +23,10 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
-bool_t sec_channel_ping_1_svc(int *result, struct svc_req *rqstp)
+int *
+sec_channel_ping_1_svc(struct svc_req *rqstp)
 {
-	bool_t retval;
+	static int  result;
 	struct sockaddr_in *addr;
 	char *ipaddr;
 	int status;
@@ -31,8 +35,7 @@ bool_t sec_channel_ping_1_svc(int *result, struct svc_req *rqstp)
 	
 	switch(pid = fork()){
 		case -1:		/*Fork failed*/
-			retval = FALSE;
-			*result = -1;
+			result = -1;
 			break;
 
 		case 0:			/* Child */
@@ -42,29 +45,16 @@ bool_t sec_channel_ping_1_svc(int *result, struct svc_req *rqstp)
 			ipaddr = (char *)inet_ntop(AF_INET, &addr->sin_addr, ipaddr, INET_ADDRSTRLEN);
 			h = (struct hostent *)gethostbyaddr(&addr->sin_addr, INET_ADDRSTRLEN, AF_INET);
 
-			//printf("%s\t%s\n", h->h_name, ipaddr);
+			printf("%s\t%s\n", h->h_name, ipaddr);
 
 			status = execl("/opt/rocks/bin/rocks", "rocks",
 				"sync","host","sharedkey",h->h_name, NULL);
 			exit(status);
 
 		default:		/* Parent */
-			retval = TRUE;
-			*result = 0;
+			result = 0;
 			break;
 		}
 
-	return retval;
-}
-
-int
-sec_channel_1_freeresult (SVCXPRT *transp, xdrproc_t xdr_result, caddr_t result)
-{
-	xdr_free (xdr_result, result);
-
-	/*
-	 * Insert additional freeing code here, if needed
-	 */
-
-	return 1;
+	return &result;
 }
