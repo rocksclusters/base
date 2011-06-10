675344b494e8685b0d7fbe4e977be88527cce011
Anoop Rajendra <anoop.rajendra@gmail.com>
2011-06-10 19:30:28 +0000

    -All grants now moved to rocks-db-perms.py -Changed ordering so that database-security  is setup before any database accesses -Bug fixes

--- src/admin/rocks-db-perms.py
+++ src/admin/rocks-db-perms.py
@@ -1,11 +1,17 @@
 #!@PYTHON@
 
-# $Id: rocks-db-perms.py,v 1.1 2011/06/08 19:47:23 anoop Exp $
+# $Id: rocks-db-perms.py,v 1.2 2011/06/10 19:30:29 anoop Exp $
 
 # @Copyright@
 # @Copyright@
 
 # $Log: rocks-db-perms.py,v $
+# Revision 1.2  2011/06/10 19:30:29  anoop
+# -All grants now moved to rocks-db-perms.py
+# -Changed ordering so that database-security
+#  is setup before any database accesses
+# -Bug fixes
+#
 # Revision 1.1  2011/06/08 19:47:23  anoop
 # Moved script for setting up rocks db table access to
 # admin scripts from node XML files
@@ -30,6 +36,22 @@ for line in f.readlines():
 		break
 f.close()
 
+# Get all the local hostname that apache is
+# allowed to connect from.
+d = MySQLdb.connect(user='root',
+	db='mysql',
+	passwd=password,
+	unix_socket='/var/opt/rocks/mysql/mysql.sock')
+
+try:
+	db = d.cursor()
+except:
+	sys.exit(-1)
+
+db.execute('select host from user where user="apache" ' +\
+	'and host!="localhost"')
+(priv_host,) = db.fetchone()
+
 # Connect to the database
 d = MySQLdb.connect(user='root',
 	db='cluster',
@@ -40,19 +62,6 @@ try:
 	db = d.cursor()
 except:
 	sys.exit(-1)
-
-# Get the private hostname either from the database
-# or from the /tmp/site.attrs file
-if db.execute('select value from global_attributes where ' +\
-	'attr="Kickstart_PrivateHostname"') != 0:
-	(priv_hostname,) = db.fetchone()
-
-else:
-	f = open('/tmp/site.attrs')
-	for line in f.readlines():
-		if line.split(':')[0].strip() == 'Kickstart_PrivateHostname':
-			priv_hostname = line.split(':')[1].strip() 
-			break
 
 # Get a list of all tables in the database
 db.execute('show tables')
@@ -74,13 +83,34 @@ for (tab_name,) in tables:
 		cmd_set.append('grant select on cluster.%s to ""@"localhost"' % tab_name)
 		cmd_set.append('grant select on cluster.%s to ""@"%%.local"' % tab_name)
 		cmd_set.append('grant select,insert,update,delete on cluster.%s to "apache"@"localhost"' % tab_name)
-		cmd_set.append('grant select,insert,update,delete on cluster.%s to "apache"@"%s"' % (tab_name, priv_hostname))
+		cmd_set.append('grant select,insert,update,delete on cluster.%s to "apache"@"%s"' % (tab_name, priv_host))
 
 # Finally grant apache the ability to lock a table if necessary
 cmd_set.append('grant lock tables on cluster.* to "apache"@"localhost"')
-cmd_set.append('grant lock tables on cluster.* to "apache"@"%s"' % priv_hostname)
+cmd_set.append('grant lock tables on cluster.* to "apache"@"%s"' % priv_host)
+
+# Do the same for procedures and functions;
+db.execute('show procedure status')
+for row in db.fetchall():
+	cmd_set.append('grant execute on procedure cluster.%s to "apache"@"localhost"' % row[1])
+	cmd_set.append('grant execute on procedure cluster.%s to ""@"localhost"' % row[1])
+	cmd_set.append('grant execute on procedure cluster.%s to ""@"%%.local"' % row[1])
+
+db.execute('show function status')
+for row in db.fetchall():
+	cmd_set.append('grant execute on function cluster.%s to "apache"@"localhost"' % row[1])
+	cmd_set.append('grant execute on function cluster.%s to ""@"localhost"' % row[1])
+	cmd_set.append('grant execute on function cluster.%s to ""@"%%.local"' % row[1])
+
+# Grant all access to temporary tables
+cmd_set.append('grant select, insert, update, delete, drop, alter, ' +\
+	'create temporary tables on temptables.* to ""@"localhost",' +\
+	'"apache"@"localhost",""@"%.local"')
 
 # Run through the command set
 for cmd in cmd_set:
-	db.execute(cmd)
-
+	try:
+		db.execute(cmd)
+	except:
+		sys.stderr.write('Could not execute "%s"\n' % cmd)
+		
