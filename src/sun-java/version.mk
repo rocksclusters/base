8e3f97e477aa9ca155d97a68dfc65ea1d213a8b1
Anoop Rajendra <anoop.rajendra@gmail.com>
2011-08-24 06:03:28 +0000

    Added JDK 6u27

--- src/sun-java/version.mk
+++ src/sun-java/version.mk
@@ -1,5 +1,5 @@
 NAME = jdk
 VERSION = 1.6.0
-RELEASE = 17
-X86_TARBALL = jdk-6u17-solaris-i586.tar.Z
-X64_TARBALL = jdk-6u17-solaris-x64.tar.Z
+RELEASE = 26
+X86_TARBALL = jdk-6u27-solaris-i586.tar.Z
+X64_TARBALL = jdk-6u27-solaris-x64.tar.Z
