58350335f1586547ec045a5e6d42767172633932
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2012-01-05 04:49:14 +0000

    Newer version of graphviz

--- src/foundation-graphviz/linux.mk
+++ src/foundation-graphviz/linux.mk
@@ -2,6 +2,7 @@ build:
 	gunzip -c graphviz-$(VERSION).tar.gz | $(TAR) -xf -
 	( 					\
 		cd graphviz-$(VERSION);		\
-		./configure --prefix=$(PKGROOT);\
+		./configure --prefix=$(PKGROOT) \
+		--enable-perl=no;\
 		$(MAKE);			\
 	)
--- src/foundation-graphviz/version.mk
+++ src/foundation-graphviz/version.mk
@@ -1,3 +1,3 @@
 NAME	= foundation-graphviz
-VERSION = 2.6
+VERSION = 2.28.0
 RELEASE = 0
