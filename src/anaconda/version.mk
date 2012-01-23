bd98dcf314e55b9efa23328ff252d8f7d17acc1d
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2012-01-23 19:52:33 +0000

    Build on 5 or 6. Exact version to be build dependent on VERSION.MAJOR of Rocks itself

--- src/anaconda/version.mk
+++ src/anaconda/version.mk
@@ -1,5 +1,12 @@
 NAME		= anaconda
-VERSION		= 11.1.2.242
 RELEASE		= 1
-
 MAKE.iscontrib  = 1
+
+ifeq ($strip $(VERSION.MAJOR), 5)
+VERSION		= 11.1.2.242
+SRPMRELEASE	= $(RELEASE)
+else
+VERSION 	= 13.21.149
+SRPMRELEASE	= $(RELEASE).el6
+endif
+
