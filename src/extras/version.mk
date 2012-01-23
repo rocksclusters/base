96edcfb9137f9706617664bfa2ffe112fa0ecfc4
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2012-01-23 20:00:27 +0000

    caching named package changed from 5 to 6. Do we really need this anymore?

--- src/extras/version.mk
+++ src/extras/version.mk
@@ -7,3 +7,9 @@ MAKE.iscontrib  = 1
 DISTPATH	= rocks-dist/$(ARCH)/RedHat/RPMS
 OS_VERSION	= $(shell mysql --batch --execute='select version from rolls where name="os" and enabled="yes"' -u apache cluster | grep -v version)
 
+
+ifeq ($strip $(VERSION.MAJOR), 5)
+CACHENAMED	= caching-nameserver
+else
+CACHENAMED	= bind-chroot
+endif
