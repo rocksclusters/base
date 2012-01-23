0ed84af43d47aa871d0aece221f3da6f524de28e
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2012-01-23 20:05:43 +0000

    The kids decided to change the locale of syslinux images. Guess the old pathname had bad karma. Proper pathing for 5 and 6

--- src/pxeflash/version.mk
+++ src/pxeflash/version.mk
@@ -4,3 +4,9 @@ RELEASE = 0
 DOSBOOT = FDSTD.288
 PXEIMAGE = $(NAME).img
 PKGROOT = /opt/pxeflash
+
+ifeq ($strip $(VERSION.MAJOR), 5)
+MEMDISK		= /usr/lib/syslinux/memdisk
+else
+MEMDISK		= /usr/share/syslinux/memdisk
+endif
