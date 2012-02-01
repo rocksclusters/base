2b97c32608edff09737d7c45fb39bd075d30555a
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2012-02-01 20:38:18 +0000

    Support Python-2.6.7 as foundation for version 6. 2.4.2 for version 5

--- src/foundation-python/version.mk
+++ src/foundation-python/version.mk
@@ -1,3 +1,13 @@
 NAME	= foundation-python
+CONFIGOPTS = --enable-ipv6 --enable-unicode=ucs4; 
+
+ifeq ($strip $(VERSION.MAJOR), 5)
 VERSION = 2.4.2
 RELEASE = 0
+ADDFLAGS = 
+else
+VERSION = 2.6.7
+RELEASE = 0
+ADDFLAGS = "CFLAGS=-fPIC"
+CONFIGOPTS += --exec-prefix=$(PKGROOT)
+endif 
