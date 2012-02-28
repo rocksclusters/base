e60a116b80bf8a9d930f1830dcff2d0db76602e2
Luca Clementi <luca.clementi@gmail.com>
2012-02-28 20:53:13 +0000

    LIBWRAP path is wrong for 5.7

--- src/ekv/version.mk
+++ src/ekv/version.mk
@@ -3,7 +3,7 @@ RELEASE		= 0
 FOUNDATION	= /opt/rocks
 
 ifeq ($(strip $(VERSION.MAJOR)), 5)
-LIBWRAP	=	/usr/$(LIB)/libwrap.so.*
+LIBWRAP	=	/usr/$(LIB)/libwrap.so*
 else
 LIBWRAP	=	/$(LIB)/libwrap.so.*
 endif
