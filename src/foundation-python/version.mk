20025932b207595def1ca04a11ba5798b98c89b6
Luca Clementi <luca.clementi@gmail.com>
2012-03-02 02:39:18 +0000

    Fix for the python unicode ucs4 problem (aka --enable-unicode=ucs4)

--- src/foundation-python/version.mk
+++ src/foundation-python/version.mk
@@ -1,5 +1,5 @@
 NAME	= foundation-python
-CONFIGOPTS = --enable-ipv6 --enable-unicode=ucs4; 
+CONFIGOPTS = --enable-ipv6 --enable-unicode=ucs4
 
 ifeq ($(strip $(VERSION.MAJOR)), 5)
 VERSION = 2.4.2
@@ -9,5 +9,5 @@ else
 VERSION = 2.6.7
 RELEASE = 0
 ADDFLAGS = "CFLAGS=-fPIC"
-CONFIGOPTS += --exec-prefix=$(PKGROOT)
+#CONFIGOPTS += --exec-prefix=$(PKGROOT)
 endif 
