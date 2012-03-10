1504d6cc47eb99b9aa020ee51a9c3e838ddf4d3d
Luca Clementi <luca.clementi@gmail.com>
2012-03-10 02:30:22 +0000

    best new future of rocks ;-) Los_Angeles first time zone when installing

--- src/screens/make-timezones.py
+++ src/screens/make-timezones.py
@@ -5,16 +5,21 @@ import string
 places = []
 file = open('/usr/share/zoneinfo/zone.tab', 'r')
 
+localTimezone=""
 for line in file.readlines():
 	l = string.split(line)
 
 	if len(l) < 2 or l[0][0] == '#':
 		continue
 
-	places.append(l[2])
+	if l[2].find("Los_Angeles") >= 0 :
+		localTimezone=l[2]
+	else:
+		places.append(l[2])
 
 file.close()
 
 places.sort()
+places.insert(0, localTimezone)
 for p in places:
 	print '<option>' + p + '</option>'
