d9dd9fe8819cbf53fbe0ca068d4f3138848bb30c
Luca Clementi <luca.clementi@gmail.com>
2013-01-05 01:42:40 +0000

    Fix updates and base path
    Updates and base path to retreive the RPM from the mirror did not include
    the last trailing / so wget in recursive mode was fetching not just
    /centos/6.3/updates/x86_64/Packages/
    but also all the other /centos/6.3/updates/x86_64/drpms/ etc.
    The drpms folder contains 10GB of stuff.
    So it fills the disk during comilation.

--- OSROLL/version.mk
+++ OSROLL/version.mk
@@ -10,13 +10,13 @@ endif
 ifeq ($(VERSION.MAJOR), 5)
 VERSION=5.8
 PKGS=RPMS
-BASEPATH=centos/$(VERSION)/os/$(ARCH)/CentOS
+BASEPATH=centos/$(VERSION)/os/$(ARCH)/CentOS/
 else
 VERSION=6.3
 PKGS=Packages
-BASEPATH=centos/$(VERSION)/os/$(ARCH)/$(PKGS)
+BASEPATH=centos/$(VERSION)/os/$(ARCH)/$(PKGS)/
 endif
 
 MIRRORURL=http://mirror.hmc.edu
-UPDATESPATH=centos/$(VERSION)/updates/$(ARCH)/$(PKGS)
+UPDATESPATH=centos/$(VERSION)/updates/$(ARCH)/$(PKGS)/
 
