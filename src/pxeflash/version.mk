cdf12b2087e3a024041476dad5513bd4b1e57267
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2013-01-25 04:34:45 +0000

    memdisk now in the same place as RHEL6/CentOS6.

--- src/pxeflash/version.mk
+++ src/pxeflash/version.mk
@@ -4,9 +4,4 @@ RELEASE = 0
 DOSBOOT = FDSTD.288
 PXEIMAGE = $(NAME).img
 PKGROOT = /opt/pxeflash
-
-ifeq ($(strip $(VERSION.MAJOR)), 5)
-MEMDISK		= /usr/lib/syslinux/memdisk
-else
 MEMDISK		= /usr/share/syslinux/memdisk
-endif
