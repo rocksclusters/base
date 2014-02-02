9136e51f07d795eaabeda362f493f0897bc91355
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2014-02-01 21:24:16 -0800

    Github doesn't support files larger than 100M. So split the jdk rpms into smaller pieces and then reassemble (cherry picked from commit 8591661b4b8e6f30986903167026ada7a13134dd)

--- src/java/i386.mk
+++ src/java/i386.mk
@@ -1,3 +1,4 @@
 rpm:: build
+	for j in `ls *.i[35]86.rpm.* | sed 's/rpm.*/rpm/' | sort | uniq`; do cat $$j.* > $$j; done 
 	-cp *.i386.rpm $(REDHAT.RPMS)/$(ARCH)/
 	-cp *.i586.rpm $(REDHAT.RPMS)/$(ARCH)/
