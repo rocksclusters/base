bc039aa3d77dba31ec6eb2adeb1d8d5779a45d0d
Luca Clementi <luca.clementi@gmail.com>
2014-03-07 15:41:30 -0800

    tests: full siute of tests for the attriubtes command line

--- tests/t2004-attributes.sh
+++ tests/t2004-attributes.sh
@@ -27,9 +27,11 @@ get_host_attr(){
 
 test_expect_success 'test attributes - set up tests' '
 	/opt/rocks/bin/rocks add host $node_name cpus=1 membership=compute\
-		os=linux rack=10 rank=10
+		os=linux rack=10 rank=10 &&
+	rocks list host 
 '
 
+# -- begin --  global
 test_expect_success 'test attributes - global attributes' '
 	rocks add attr $attr_name globa &&
 	test `get_attr "$attr_name"` = "globa" &&
@@ -37,33 +39,86 @@ test_expect_success 'test attributes - global attributes' '
 	test `get_attr "$attr_name"` = "global" &&
 	test `get_host_attr localhost "$attr_name"` = "global" 
 '
-# rocks dump attr | grep " $attr_name "
-
 test_expect_success 'test attributes - global attributes add duplicate' '
 	rocks add attr $attr_name global || true
 '
-
 test_expect_success 'test attributes - global attributes dump' '
 	rocks dump attr | grep " $attr_name " 
 '
+# -- end -- global
 
+
+# -- begin --  os
+test_expect_success 'test attributes - os attributes' '
+	rocks add os attr linux $attr_name os &&
+	test `get_host_attr $node_name $attr_name` = "os" &&
+	test `get_host_attr localhost $attr_name` = "os"
+'
+test_expect_success 'test attributes - os attributes add duplicate' '
+	rocks add os attr linux $attr_name os2 || true
+'
+test_expect_success 'test attributes - os attributes set' '
+	rocks set os attr linux $attr_name os2 &&
+	test `get_host_attr $node_name ${attr_name}_old` = "os" &&
+	test `get_host_attr $node_name ${attr_name} ` = "os2" &&
+	rocks set os attr linux $attr_name os
+'
+test_expect_success 'test attributes - os attributes dump' '
+        rocks dump os attr | grep " $attr_name " | grep os
+'
+# -- end -- os
+
+
+# -- begin --  appliance
+test_expect_success 'test attributes - appliance attributes' '
+	rocks add appliance attr compute $attr_name appliance &&
+	test `get_host_attr $node_name $attr_name` = "appliance" &&
+	test `get_host_attr localhost $attr_name` = "os"
+'
+test_expect_success 'test attributes - appliance attributes add duplicate' '
+	rocks add appliance attr compute $attr_name appliance2 || true
+'
+test_expect_success 'test attributes - appliance attributes set' '
+	rocks set appliance attr compute $attr_name appliance2 &&
+	test `get_host_attr $node_name ${attr_name}_old` = "appliance"
+'
+test_expect_success 'test attributes - appliance attributes dump' '
+        rocks dump appliance attr | grep " $attr_name " | grep appliance
+'
+# -- end -- appliance
+
+
+# -- begin --  host
 test_expect_success 'test attributes - host attributes' '
 	rocks add host attr $node_name $attr_name host &&
 	test `get_host_attr $node_name $attr_name` = "host" &&
-	test `get_host_attr localhost $attr_name` = "global"
+	test `get_host_attr localhost $attr_name` = "os"
 '
-
 test_expect_success 'test attributes - host attributes add duplicate' '
 	rocks add host attr $node_name $attr_name host2 || true
 '
-
+test_expect_success 'test attributes - host attributes set' '
+	rocks set host attr $node_name $attr_name host2 &&
+	test `get_host_attr $node_name ${attr_name}_old` = "host" &&
+	test `get_host_attr $node_name ${attr_name}` = "host2"
+'
 test_expect_success 'test attributes - host attributes dump' '
-        rocks dump host attr | grep " $attr_name "
+        rocks dump host attr | grep " $attr_name " | grep host2
+'
+# -- end -- host
+
+test_expect_success 'test attributes - rocks report host attr' '
+	rocks report host attr $node_name | grep "^$attr_name:" | grep host2
 '
 
-test_expect_success 'test attributes - tear down' '
+test_expect_success 'test attributes - tear down removing attribute' '
 	rocks remove attr $attr_name &&
-	rocks remove host attr $attr_name &&
+	rocks remove os attr $attr_name &&
+	rocks remove appliance attr $attr_name &&
+	rocks remove host attr $attr_name 
+'
+
+test_expect_success 'test attributes - tear down removing facke host' ' 
 	rocks remove host $node_name
 '
 
