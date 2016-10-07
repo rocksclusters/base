2b34554cefaaa5f4bab02d7920c0a11ccd2ad0c4
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2016-10-07 16:09:45 -0700

    protobuf now uses RPM.FILES

--- src/protobuf/version.mk
+++ src/protobuf/version.mk
@@ -1,7 +1,8 @@
 PKGROOT		= /usr
 NAME    	= protobuf
 VERSION 	= 2.6.1
-RELEASE 	= 1
+RELEASE 	= 2
 GTEST		= googletest-release
 GTEST_RELEASE	= 1.5.0
 TARBALL_POSTFIX	= tar.gz
+RPM.FILES = "/usr/bin/*\\n/usr/include/google\\n/usr/lib64/lib*\\n/usr/lib64/pkgconfig/*"
