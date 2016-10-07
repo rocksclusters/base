6cd80fd1123d22b84d8d686e105859da9e02ae3f
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2016-10-07 16:13:13 -0700

    qrencode now uses RPM.FILES

--- src/qrencode/version.mk
+++ src/qrencode/version.mk
@@ -2,5 +2,6 @@
 PKGROOT		= /usr
 NAME    	= qrencode
 VERSION 	= 3.4.0
-RELEASE 	= 1
+RELEASE 	= 2
 TARBALL_POSTFIX	= tar.bz2
+RPM.FILES	= "/usr/bin/*\\n/usr/include/*\\n/usr/lib/lib*\\n/usr/lib/pkgconfig/*\\n/usr/share/man/man1/*"
