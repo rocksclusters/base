591ed0e0e6ee001bd4eaadac072d68b0c869bb45
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2016-10-08 07:42:43 -0700

    Use RPM.FILES to specify payload

--- src/411-master/version.mk
+++ src/411-master/version.mk
@@ -1,4 +1,10 @@
 NAME = rocks-411-master
-RELEASE = 0
+RELEASE = 1
 RPM.ARCH = noarch
 RPM.FILE.EXTRAS = "%config /var/411/Files.mk"
+RPM.FILES = \
+/etc/411.d \\n\
+/etc/rc.d/init.d/* \\n\
+/opt/rocks/sbin/* \\n\
+/opt/rocks/var/plugins/411 \\n\
+/var/411
