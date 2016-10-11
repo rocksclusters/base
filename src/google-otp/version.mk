5d7ca8f51779477b32ff92eca260a9bf38deda62
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2016-10-11 13:20:48 -0700

    use RPM.FILES

--- src/google-otp/version.mk
+++ src/google-otp/version.mk
@@ -1,3 +1,10 @@
 NAME = google-authenticator
 VERSION = 7SEP2012
 RELEASE = 0
+RPM.FILES = \
+/etc/pam.d/* \\n\
+/export/google-authenticator \\n\
+/lib64/security/pam_google_authenticator.so \\n\
+/opt/rocks/lib/python2*/site-packages/rocks/commands/sync/users/* \\n\
+/opt/rocks/var/plugins/411/* \\n\
+/usr/bin/*
