4fc12e81fcbfb72b3d0f6b389f7e7362311417e0
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2016-10-11 18:17:57 -0700

    Add RPM.DESCRIPTION

--- src/pdsh/version.mk
+++ src/pdsh/version.mk
@@ -7,3 +7,8 @@ RPM.FILES 	= \
 /etc/ld.so.conf.d/* \\n \
 /etc/profile.d/* \\n \
 /opt/pdsh
+RPM.DESCRIPTION = \
+pdsh is  a multithreaded remote shell client which executes commands on \\n\
+multiple remote hosts in parallel.  Pdsh can use several different \\n\
+remote shell services, including standard "rsh", Kerberos IV, and ssh.
+
