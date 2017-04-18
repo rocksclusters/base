e49667a7d5f23c7823045bd39d8acc9ccc487d7a
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2017-04-18 09:14:50 -0700

    update epel-release rpm. Also updates fail2ban rpm for EL7

--- src/yum-extras/version.mk
+++ src/yum-extras/version.mk
@@ -1,7 +1,12 @@
 ifeq ($(VERSION.MAJOR),5)
 REPORPMS.NOARCH += elrepo-release-5-3.el5.elrepo.noarch.rpm
 REPORPMS.NOARCH += epel-release-5-4.noarch.rpm
-else
+endif
+ifeq ($(VERSION.MAJOR),6)
 REPORPMS.NOARCH += elrepo-release-6-4.el6.elrepo.noarch.rpm
 REPORPMS.NOARCH += epel-release-6-5.noarch.rpm
+endif
+ifeq ($(VERSION.MAJOR),7)
+# REPORPMS.NOARCH += elrepo-release-6-4.el6.elrepo.noarch.rpm
+REPORPMS.NOARCH += epel-release-7-9.noarch.rpm
 endif
