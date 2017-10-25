06bd286fdc49e6ba8aff1f4ea1d878324c9a0351
Philip Papadopulos <ppapadopoulos@ucsd.edu>
2017-10-24 22:21:13 -0400

    GIDMIN/MAX UIDMIN/MAX need to be in the Plugin class, not the Service411 class. Caused group and passwd filters to fail

--- src/411/rocks/service411.py
+++ src/411/rocks/service411.py
@@ -391,19 +391,6 @@ class Service411:
 		# Use Blowfish with fast Cipher Block Chaining.
 		self.sym = POW.Symmetric(POW.BF_CBC)
 
-		# Use defaults found it /etc/login.def
-		try:
-			f = open("/etc/login.defs","r")
-			l = f.readlines()
-			self.UIDMIN = int(filter(lambda x : re.match('^UID_MIN',x), l)[0].strip().split()[1])
-			self.UIDMAX = int(filter(lambda x : re.match('^UID_MAX',x), l)[0].strip().split()[1])
-			self.GIDMIN = int(filter(lambda x : re.match('^GID_MIN',x), l)[0].strip().split()[1])
-			self.GIDMAX = int(filter(lambda x : re.match('^GID_MAX',x), l)[0].strip().split()[1])
-		except:
-			self.UIDMIN = 1000
-			self.UIDMAX = 60000
-			self.GIDMIN = self.UIDMIN
-			self.GIDMAX = self.UIDMAX
 
 	def fillPool(self):
 		"""Starts the random pool. Dont do in constructor since this
@@ -865,6 +852,19 @@ class Service411:
 class Plugin:
 	def __init__(self, attrs = {}):
 		self.attrs = attrs
+		# Use defaults found it /etc/login.def
+		try:
+			f = open("/etc/login.defs","r")
+			l = f.readlines()
+			self.UIDMIN = int(filter(lambda x : re.match('^UID_MIN',x), l)[0].strip().split()[1])
+			self.UIDMAX = int(filter(lambda x : re.match('^UID_MAX',x), l)[0].strip().split()[1])
+			self.GIDMIN = int(filter(lambda x : re.match('^GID_MIN',x), l)[0].strip().split()[1])
+			self.GIDMAX = int(filter(lambda x : re.match('^GID_MAX',x), l)[0].strip().split()[1])
+		except:
+			self.UIDMIN = 1000
+			self.UIDMAX = 60000
+			self.GIDMIN = self.UIDMIN
+			self.GIDMAX = self.UIDMAX
 		pass
 	
 class NodeFilter(xml.dom.NodeFilter.NodeFilter):
--- src/411/version.mk
+++ src/411/version.mk
@@ -1,5 +1,5 @@
 NAME = rocks-411
-RELEASE = 1
+RELEASE = 2
 RPM.ARCH = noarch
 RPM.FILES = \
 /opt/rocks/bin/*\\n\
