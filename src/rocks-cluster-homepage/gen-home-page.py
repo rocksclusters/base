46d84fcbc8e27c096ac03db6fb163bd16957845c
nadyawilliams <nadya@sdsc.edu>
2017-11-30 15:48:38 -0800

    add styling

--- src/rocks-cluster-homepage/gen-home-page.py
+++ src/rocks-cluster-homepage/gen-home-page.py
@@ -5,52 +5,83 @@
 import os
 import subprocess
 import os.path
+import sys
+from operator import itemgetter
+
+def colInfo(r):
+        uguidepath=os.path.join('/roll-documentation',r[0],r[1])
+        fullpath="/var/www/html/" + uguidepath
+        if os.path.exists(fullpath):
+                listelem=withguide % (r[0],r[1],uguidepath)
+        else:
+                listelem=withoutguide % (r[0],r[1])
+        return  listelem
 
 template = """
 <!DOCTYPE html>
 <html>
+<head>
+<style>
+h2 { color: #016c93; }
+.title {color: #0190c4; font-weight: bold;}
+.cr {color: #7b8599; font-weight: bold;}
+.tcolor { background-color: #e7efff;}
+</style>
+</head>
 <body>
 
-<h1>
-<img src="/images/rocks-logo.png" alt="Rocks Logo" width="139" height="139"
-align="center"/>
-Welcome to the %s cluster home page </h1>
-<p> If <a href=\"http://ganglia.sourceforge.net\">ganglia</a> is
-installed on your cluster, visit your <a href="/ganglia"> local monitoring 
-instance</a> for information 
-</p>
+<table>
+  <tr>
+    <td valign="top">
+      <img src="/images/rocks-logo.png" alt="Rocks Logo" width="100" />
+    </td>
+    <td>
+      <h2>Welcome to the <i>%s</i> cluster home page</h2>
+      <font class="title"><h3>Cluster Information</h3></font>
+      <ul>
+        <li><a href="/ganglia">Local ganglia monitoring instance</a> (if ganglia roll is installed)</li>
+        <li><a href=\"/misc/dot-graph.php/\">Cluster kickstart graph</a>
+        <li>Cluster contact: %s</li>
+      </ul>
+      <p>Visit <a href=\"http://www.rocksclusters.org\">Rocks Website</a> for latest news and updates </p>
+    </td>
+  </tr>
+</table>
 
-<p> You can <a href=\"/misc/dot-graph.php/\"> view your kickstart graph</a>
-
-<p> The cluster contact is: %s</p>
-
-<p> The <a href=\"http://www.rocksclusters.org\">Rocks rolls</a> installed are:
-<ul>
+<table class="tcolor">
+<tr><th colspan="4" bgcolor=""><font class="title"><h3>Installed Rolls</h3></font></th></tr>
 %s
-<ul>
-</p>
-<p> (C) 2003 - 2017 UC Regents </p>
+</table>
+
+<p class="cr">(C) Copyright 2003 - 2017 UC Regents</p>
+
 </body>
 </html>
 """
 
 ## The following are templates for listing the installed rolls
-listtemplate="    <li>%s</li>\n"
-withguide="%s (version %s) <a href=\"%s\">(roll usersguide)</a>"
-withoutguide="%s (version %s)"
-output=subprocess.check_output(["rocks","list","roll","output-header=no"]).replace(':','').split('\n')
-rolls=filter(lambda x: len(x) > 0, map(lambda x: x.split(), output))
+withguide = "<td>&emsp;%s</td><td>(v.%s)&emsp;<a href=\"%s\">roll usersguide</a>&emsp;</td>"
+withoutguide = "<td>&emsp;%s</td><td>(v.%s)</td>"
 
+output = subprocess.check_output(["rocks","list","roll","output-header=no"]).replace(':','').split('\n')
+rolls = filter(lambda x: len(x) > 0, map(lambda x: x.split(), output))
+rolls = sorted(rolls, key=itemgetter(0))
+
+all=len(rolls)
+rows=len(rolls)/2
 rollList=""
-for r in rolls:
-	uguidepath=os.path.join('/roll-documentation',r[0],r[1])
-	fullpath="/var/www/html/" + uguidepath
-	if os.path.exists(fullpath):
-		listelem=withguide % (r[0],r[1],uguidepath)
-	else:
-		listelem=withoutguide % (r[0],r[1])
-
-	rollList += listtemplate % listelem
+i = 0
+while i <= rows :
+    second = i+rows
+    if second <= all-1  :
+       if i == rows:
+           col2 = colInfo(rolls[second])
+           rollList += "<tr>\n<td colspan='2'> </td>\n%s</tr>\n" % (col2)
+       else:
+           col1 = colInfo(rolls[i])
+           col2 = colInfo(rolls[second])
+           rollList += "<tr>\n%s%s</tr>\n" % (col1, col2)
+    i += 1
 
 contact = subprocess.check_output(["rocks","report","host", "attr", "localhost", "attr=Info_ClusterContact"]).strip()
 clustername = subprocess.check_output(["rocks","report","host", "attr", "localhost", "attr=Info_ClusterName"]).strip()
